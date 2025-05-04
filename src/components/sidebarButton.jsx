3ceff43ff50e3e2202bf84ae9aa4ee8fa708c6c6
Sanket Botre <sanketbotre24@gmail.com>
2025-05-05 01:53:01 +0530

    unnecessary files removed

--- src/components/sidebarButton.jsx
+++
@@ -1,8 +0,0 @@
-export default function sidebarbutton(){
-    const [collapsed, setCollapsed] = useState(false);
-    return(
-        <button className="collapse-toggle" onClick={() => setCollapsed(!collapsed)}>
-            {collapsed ? '➡️' : '⬅️'}
-        </button>
-    );
-}
